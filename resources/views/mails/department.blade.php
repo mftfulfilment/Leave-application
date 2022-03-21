a170f4d1c44398e7990c750f965b2947219e19e2
Jimmy <jimkiarie8@gmail.com>
2022-03-21 11:40:07 +0300

    commit

--- resources/views/mails/department.blade.php
+++ resources/views/mails/department.blade.php
@@ -12,7 +12,9 @@ The body of your message.
     <li class="list-group-item">Person to take charge <b style="float: right">{{ $application->take_charge }}</b></li>
     <li class="list-group-item">Leave Start Date <b style="float: right">{{ $application->start_date }}</b></li>
     <li class="list-group-item">Leave End Date <b style="float: right">{{ $application->end_date }}</b></li>
+    @if ($application->attachment->path )
     <li class="list-group-item">Attachment <b style="float: right"><a href="{{ env('APP_URL') . '/' .  $application->attachment->path }}" target="_blank">Attachment</a></b></li>
+    @endif
 </ul>
 @component('mail::button', ['url' => $url . '/approved'])
 Approve
